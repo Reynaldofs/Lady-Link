0e9101d45c0b3248da5bfc1c76c5c26bedfd437d
lukas-95 <lucrecmen@gmail.com>
2023-05-17 15:47:38 -0300

    add CRUD for Postagens entity

--- src/main/java/com/generation/ladylink/repository/PostagemRepository.java
+++ src/main/java/com/generation/ladylink/repository/PostagemRepository.java
@@ -3,7 +3,13 @@ package com.generation.ladylink.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.generation.ladylink.model.Postagem;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface PostagemRepository extends JpaRepository <Postagem, Long>{
+
+    public List <Postagem> findAllByTituloContainingIgnoreCase(
+            @Param("titulo") String titulo);
 
 }
